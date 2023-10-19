88c762e791a3222597886460ff82e85bcccf5ddf
Infortech07 <Techpilot07@outlook.com>
2023-10-19 17:51:20 -0600

    front-payroll, agregando direccion de produccion

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 export const config = {
     nextAuthUrl: process.env.NEXTAUTH_URL || "http://localhost:3000",
     nextAuthSecret: process.env.NEXTAUTH_SECRET || "secret",
-    API_URL: process.env.API_URL || "http://localhost:8000/api/v1/",
+    //API_URL: process.env.API_URL || "http://localhost:8000/api/v1/",
+    API_URL: "http://18.216.107.32/api/v1/",
     API_URL_STORE: "https://fakestoreapi.com/products",
     ENVIROMENT: process.env.NODE_ENV || "development",
 };
