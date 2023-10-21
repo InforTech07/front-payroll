2ca929279b67e21b08fd2e5d7691a90248602a24
Infortech07 <Techpilot07@outlook.com>
2023-10-21 02:02:27 -0600

    Actualizando boton de registro

--- src/components/blog/header.tsx
+++ src/components/blog/header.tsx
@@ -18,7 +18,7 @@ function HeaderBlog(){
                             <Link href="/register-company"
                             className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
                             >
-                            <span className="relative text-base font-semibold text-white">iniciar</span>
+                            <span className="relative text-base font-semibold text-white">Prueba Gratis</span>
                             </Link>
                             <Link
                             href="#"
